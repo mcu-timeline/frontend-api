2f81a86925571fb90c84ed9262429422d2eda7fe
Nikodem Wrona <wronanikodem@protonmail.com>
2022-11-21 16:46:27 +0100

    mvp: add content graphql api federation

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'content', url: 'http://localhost:3001/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
